fd1e1da609e1fc64c7566ab8973ac0b9d1693e65
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-05-10 19:44:00 +0600

    hash() method

--- src/main/java/org/example/MyHashTable.java
+++ src/main/java/org/example/MyHashTable.java
@@ -2,9 +2,9 @@ package org.example;
 
 public class MyHashTable<K, V> {
     private class HashNode<K, V>{
-        private K key;
-        private V value;
-        private HashNode<K, V> next;
+        private K key; //key for the hash table entry
+        private V value; //value associated with the key
+        private HashNode<K, V> next; //reference to the next HashNode
         public HashNode(K key, V value){
             this.key = key;
             this.value = value;
@@ -15,17 +15,20 @@ public class MyHashTable<K, V> {
         }
     }
     private HashNode<K, V>[] chainArray;
-    private int M = 11;
-    private int size;
+    private int M = 11; //size of the hash table, which is set to 11 by default
+    private int size; //the number of key-value pairs in the hash table
     public MyHashTable(){
+        //the default constructor with the default size of 11
         chainArray = new HashNode[M];
     }
     public MyHashTable(int M){
+        //the constructor takes an integer and sets M to the give value
         this.M = M;
         chainArray = new HashNode[M];
     }
     private int hash(K key){
-
+        int hashCode = key.hashCode();
+        int index = Math.abs(hashCode) % M;
     }
     public void put(K key, V value){
 
